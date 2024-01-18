5d62272c3e6365f5dd7b39018e013e02b2662f19
allisonfintz <117038166+allisonfintz@users.noreply.github.com>
2024-01-18 18:11:57 -0500

    Update BoxedString.cpp

--- BoxedString.cpp
+++ BoxedString.cpp
@@ -1,27 +1,26 @@
 #include "string"
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 void BoxedString(vector<string> a){
-    int max = 0;
+    size_t m = 0;
     //find longest string
     for(string str:a) {
-        if (str.size() > max)
-            max = str.size();
+        m = max(m, str.size());
     }
     string startend = "****";
     string temp = "";
     //create top and bottom of rectangle
-    for(int i=0; i<max; i++){
+    for(int i=0; i<m; i++){
         startend += "*";
         temp += " ";
     }
     cout << startend << endl;
     //create rectangle
-    for(int i = 0; i< a.size(); i++){
-        cout << "* " << a[i] << temp.substr(0, max-a[i].size()) << " *" << endl;
-    }
+    for(int i = 0; i< a.size(); i++)
+        cout << "* " << a[i] << temp.substr(0, m-a[i].size()) << " *" << endl;
     cout << startend << endl;
 }
 int main(){
