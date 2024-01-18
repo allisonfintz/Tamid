7d117324f020cab2255c56631ef6d7063139527d
allisonfintz <117038166+allisonfintz@users.noreply.github.com>
2024-01-18 17:52:33 -0500

    Update BoxedString.cpp

--- BoxedString.cpp
+++ BoxedString.cpp
@@ -5,17 +5,20 @@ using namespace std;
 
 void BoxedString(vector<string> a){
     int max = 0;
+    //find longest string
     for(string str:a) {
         if (str.size() > max)
             max = str.size();
     }
     string startend = "****";
     string temp = "";
+    //create top and bottom of rectangle
     for(int i=0; i<max; i++){
         startend += "*";
         temp += " ";
     }
     cout << startend << endl;
+    //create rectangle
     for(int i = 0; i< a.size(); i++){
         cout << "* " << a[i] << temp.substr(0, max-a[i].size()) << " *" << endl;
     }
